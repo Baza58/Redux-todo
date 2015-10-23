993d80bf4ea055067e4f8e847e64b2015d1837ad
Petr Bažout <bazout.p@gmail.com>
2015-10-23 10:56:24 +0200

    Add todo constants

---
+++ constants/todo-constants.js
@@ -0,0 +1,6 @@
+export const ADD_TODO = 'ADD_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
+export const COMPLETE_ALL = 'COMPLETE_ALL';
+export const UNDO_ALL = 'UNDO_ALL';
