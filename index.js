582a9f030356833dc4350e31f8b4fb5f9e0df7f6
Petr Bažout <bazout.p@gmail.com>
2015-10-23 11:51:55 +0200

    Add entry point code

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
+import App from './containers/app';
+
+const store = configureStore();
+
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+, document.getElementById('app'));
