a3de4247d48613b4f9594ccc544f75082948950d
Petr Bažout <bazout.p@gmail.com>
2015-10-25 15:14:11 +0100

    Add react-transform

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,8 +4,8 @@ var webpack = require('webpack');
 module.exports = {
 	devtool: 'eval',
     entry: [
-    	'webpack-dev-server/client?http://localhost:3000',
-    	'webpack/hot/only-dev-server',
+  
+    	'webpack-hot-middleware/client',
     	'./index'
   ],
 	output: {
@@ -14,13 +14,14 @@ module.exports = {
 		publicPath: '/static/'
 	},
 	plugins: [
-    	new webpack.HotModuleReplacementPlugin()
+    	new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
   	],
 	module: {
 		loaders: [
 		{
 			test: /\.js$/,
-			loaders: ['react-hot','babel?stage=0'],
+			loaders: ['babel'],
 			exclude: /node_modules/,
 		},
 		{
